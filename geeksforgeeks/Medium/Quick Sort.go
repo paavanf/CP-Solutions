e5e51274c7dbfedfceedcbdf1957d9c7127626cd
paavanf <pavu0504@gmail.com>
2026-08-23 20:47:15 +0530

    Add Quick Sort (Medium)

---
+++ geeksforgeeks/Medium/Quick Sort.go
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    void quickSort(vector<int>& arr, int low, int high) {
+        // code here
+        if(low<high)
+        {
+            int p=partition(arr,low,high);
+            quickSort(arr,low,p-1);
+            quickSort(arr,p+1,high);
+        }
+    }
+
+    int partition(vector<int>& arr, int low, int high) {
+        // code here
+        int pivot=arr[high];
+        int i=low-1;
+        for(int j=low;j<high;j++)
+        {
+            if(arr[j]<=pivot)
+            {
+                i++;
+                swap(arr[i],arr[j]);
+            }
+        }
+        swap(arr[i+1],arr[high]);
+        return i+1;
+    }
+};
