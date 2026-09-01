732d1c5d877c4158f93bca263b5f48c0f1a49f1e
paavanf <pavu0504@gmail.com>
2026-09-01 20:41:32 +0530

    Add Lexicographically-Smallest-Permutation-Greater-Than-Target (hash-table)

--- leetcode/hash-table/3720-Lexicographically-Smallest-Permutation-Greater-Than-Target.cpp
+++ leetcode/hash-table/3720-Lexicographically-Smallest-Permutation-Greater-Than-Target.cpp
@@ -1,39 +1,47 @@
+/*For the answer to be just greater than target:
+1)Match target as long as possible.
+2)If possible, replace the current character with the smallest larger available character.
+3)Otherwise, go backward and try replacing a previous matched character.
+4)Put all remaining characters in sorted order.
+*/
 class Solution {
 public:
     string lexGreaterPermutation(string s, string target) {
         int n=s.size();
-        vector<int>ct(26,0);
+        vector<int>fq(26,0);
         for(char c:s)
-        ct[c-'a']++;
-        //now match the target character by character from left to right
-        int match=0;
-        while(match<n && ct[target[match]-'a']>0)
+        fq[c-'a']++;
+        //first we will try to match the target
+        int i=0;
+        while(i<n && fq[target[i]-'a']>0)
         {
-            ct[target[match]-'a']--;
-            match++;
+            fq[target[i]-'a']--;
+            i++;
         }
-        if(match==n)
+        //now posi [0..i-1] match the target
+        //we make i+1th posi's character just +1
+        //if it is not possible we will backtrack
+        for(int j=i;j>=0;j--)
         {
-            match--;
-            ct[target[match]-'a']++;
-        }
-        for(int i=match;i>=0;i--)
-        {
-            for(char c=target[i]+1;c<='z';c++)
+            if(j<i)
+            fq[target[j]-'a']++;
+            //find smallest character greater than target[j]
+            if(j<n)
             {
-                if(ct[c-'a']>0)
+                for(int ch=target[j]-'a'+1;ch<26;ch++)
                 {
-                    ct[c-'a']--;
-                    string ans=target.substr(0,i)+c;
-                    for(char ch='a';ch<='z';ch++)
+                    if(fq[ch]>0)
                     {
-                        ans.append(ct[ch-'a'],ch);
+                        string ans=target.substr(0,j);
+                        ans+=char('a'+ch);
+                        fq[ch]--;
+                        //add remaining characters in sorted order
+                        for(int k=0;k<26;k++)
+                        ans+=string(fq[k],'a'+k);
+                        return ans;
                     }
-                    return ans;
                 }
             }
-            if(i>0)
-            ct[target[i-1]-'a']++;
         }
         return "";
     }
