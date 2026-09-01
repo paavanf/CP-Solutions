22fdac95af7fad9118963b3e795bec64e25eaa5f
paavanf <pavu0504@gmail.com>
2026-09-01 20:32:48 +0530

    Add Range-Sum-Query-2D---Immutable (array)

---
+++ leetcode/array/304-Range-Sum-Query-2D---Immutable.cpp
@@ -0,0 +1,26 @@
+class NumMatrix {
+public:
+    vector<vector<int>>pref;
+    NumMatrix(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        pref.resize(m+1,vector<int>(n+1,0));
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                pref[i][j]=matrix[i-1][j-1]+pref[i-1][j]+pref[i][j-1]-pref[i-1][j-1];
+            }
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return pref[row2+1][col2+1]-pref[row1][col2+1]-pref[row2+1][col1]+pref[row1][col1];
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
