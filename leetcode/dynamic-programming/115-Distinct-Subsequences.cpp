3e63cb8e467d7af64ac37f93401cba235273c272
paavanf <pavu0504@gmail.com>
2026-09-06 21:01:37 +0530

    Add Distinct-Subsequences (dynamic-programming)

--- leetcode/dynamic-programming/115-Distinct-Subsequences.cpp
+++ leetcode/dynamic-programming/115-Distinct-Subsequences.cpp
@@ -3,21 +3,16 @@ public:
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
-        vector<vector<unsigned long long>>dp(n+1,vector<unsigned long long>(m+1,0));
-        for(int i=0;i<=n;i++)
-        dp[i][m]=1;
-        //go backwards
+        vector<unsigned long long>dp(m+1,0);
+        dp[m]=1;
         for(int i=n-1;i>=0;i--)
         {
-            for(int j=m-1;j>=0;j--)
+            for(int j=0;j<m;j++)
             {
-                unsigned long long notTake=dp[i+1][j];
-                unsigned long long take=0;
                 if(s[i]==t[j])
-                take=dp[i+1][j+1];
-                dp[i][j]=notTake+take;
+                dp[j]+=dp[j+1];
             }
         }
-        return dp[0][0];
+        return dp[0];
     }
 };
