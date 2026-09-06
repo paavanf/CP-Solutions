2c787d50fae3e46d96eebf382dcc0cfe24e87d86
paavanf <pavu0504@gmail.com>
2026-09-06 20:58:47 +0530

    Add Distinct-Subsequences (dynamic-programming)

--- leetcode/dynamic-programming/115-Distinct-Subsequences.cpp
+++ leetcode/dynamic-programming/115-Distinct-Subsequences.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    int f(int i,int j,string &s,string &t,vector<vector<int>>&dp)
-    {
-        int n=s.size();
-        int m=t.size();
-        if(j==m)
-        return 1;
-        if(i==n)
-        return 0;
-        if(dp[i][j]!=-1)
-        return dp[i][j];
-        int notTake=f(i+1,j,s,t,dp);
-        int take=0;
-        if(s[i]==t[j])
-        take=f(i+1,j+1,s,t,dp);
-        return dp[i][j]=take+notTake;
-    }
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(0,0,s,t,dp);
+        vector<vector<unsigned long long>>dp(n+1,vector<unsigned long long>(m+1,0));
+        for(int i=0;i<=n;i++)
+        dp[i][m]=1;
+        //go backwards
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=m-1;j>=0;j--)
+            {
+                unsigned long long notTake=dp[i+1][j];
+                unsigned long long take=0;
+                if(s[i]==t[j])
+                take=dp[i+1][j+1];
+                dp[i][j]=notTake+take;
+            }
+        }
+        return dp[0][0];
     }
 };
