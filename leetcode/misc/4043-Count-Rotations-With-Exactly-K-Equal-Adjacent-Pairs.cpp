6502a9d7f4f6735a5b692d06dc23fb9efb949ebb
paavanf <pavu0504@gmail.com>
2026-09-06 21:11:04 +0530

    Add Count-Rotations-With-Exactly-K-Equal-Adjacent-Pairs (misc)

---
+++ leetcode/misc/4043-Count-Rotations-With-Exactly-K-Equal-Adjacent-Pairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countRotations(string s, int k) {
+        int n=s.size();
+        int score=0;
+        for(int i=0;i<n-1;i++)
+        {
+            if(s[i]==s[i+1])
+            score++;
+        }
+        if(s[n-1]==s[0])
+        score++;
+        if(score==k)
+        return n-score;
+        if(score==k+1)
+        return score;
+        return 0;
+    }
+};
