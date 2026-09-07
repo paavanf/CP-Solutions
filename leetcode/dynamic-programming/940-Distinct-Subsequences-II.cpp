71794e73db4ebb4ccaa5dd56c6c9416d691294cf
paavanf <pavu0504@gmail.com>
2026-09-07 11:15:23 +0530

    Add Distinct-Subsequences-II (dynamic-programming)

--- leetcode/dynamic-programming/940-Distinct-Subsequences-II.cpp
+++ leetcode/dynamic-programming/940-Distinct-Subsequences-II.cpp
@@ -2,30 +2,24 @@ using ll=long long;
 const int mod=1e9+7;
 class Solution {
 public:
-    int f(int i,string &s,vector<int>&dp)
-    {
-        int n=s.size();
-        //if only empty seq remains
-        if(i==n)
-        return 1;
-        if(dp[i]!=-1)
-        return dp[i];
-        ll ans=1;//empty subseq
-        vector<bool>used(26,false);
-        for(int j=i;j<n;j++)
-        {
-            //use only first occurence of each and every character
-            if(!used[s[j]-'a'])
-            {
-                used[s[j]-'a']=true;
-                ans=(ans+f(j+1,s,dp))%mod;
-            }
-        }
-        return dp[i]=ans;
-    }
     int distinctSubseqII(string s) {
         int n=s.size();
-        vector<int>dp(n,-1);
-        return (f(0,s,dp)-1+mod)%mod;//subtracting the empty subsequence
+        vector<ll>dp(n+1,0);
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--)
+        {
+            ll ans=1;
+            vector<bool>used(26,false);
+            for(int j=i;j<n;j++)
+            {
+                if(!used[s[j]-'a'])
+                {
+                    used[s[j]-'a']=true;
+                    ans=(ans+dp[j+1])%mod;
+                }
+            }
+            dp[i]=ans;
+        }
+        return (dp[0]-1+mod)%mod;
     }
 };
