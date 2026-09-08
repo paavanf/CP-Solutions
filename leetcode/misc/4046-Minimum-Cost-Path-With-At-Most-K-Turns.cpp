264396bcdd2d3c46b08d189f4b2acbbceb328477
paavanf <pavu0504@gmail.com>
2026-09-08 19:50:22 +0530

    Add Minimum-Cost-Path-With-At-Most-K-Turns (misc)

--- leetcode/misc/4046-Minimum-Cost-Path-With-At-Most-K-Turns.cpp
+++ leetcode/misc/4046-Minimum-Cost-Path-With-At-Most-K-Turns.cpp
@@ -1,57 +1,44 @@
-using ll=long long;
-using T=tuple<ll,int,int,int,int>;//cost,r,c,dir,turn
 class Solution {
 public:
     bool path(int r,int c,int m,int n)
     {
         return r>=0 && r<m && c>=0 && c<n;
     }
+    int f(int i,int j,int turns,int prevDir,vector<vector<int>>&grid,vector<vector<vector<vector<int>>>>&dp)
+    {
+        int m=grid.size();
+        int n=grid[0].size();
+        if(!path(i,j,m,n))
+        return INT_MAX;
+        if(turns<0)
+        return INT_MAX;
+        if(i==m-1 && j==n-1)
+        return grid[i][j];
+        if(dp[i][j][turns][prevDir]!=-1)
+        return dp[i][j][turns][prevDir];
+        int dr[4]={-1,1,0,0};
+        int dc[4]={0,0,-1,1};
+        int ans=INT_MAX;
+        for(int x=0;x<4;x++)
+        {
+            int newTurns=turns;
+            if(prevDir!=4 && x!=prevDir)
+            newTurns--;
+            int nr=i+dr[x];
+            int nc=j+dc[x];
+            int nextCost=f(nr,nc,newTurns,x,grid,dp);
+            if(nextCost!=INT_MAX)
+            ans=min(ans,grid[i][j]+nextCost);
+        }
+        return dp[i][j][turns][prevDir]=ans;
+    }
     int minCost(vector<vector<int>>& grid, int k) {
         int m=grid.size();
         int n=grid[0].size();
-        if(m==1 && n==1)
-        return grid[0][0];
-        vector<vector<vector<vector<ll>>>>dist(m,vector<vector<vector<ll>>>(n,vector<vector<ll>>(4,vector<ll>(k+1,LLONG_MAX))));
-        priority_queue<T,vector<T>,greater<T>>pq;
-        int dr[4]={-1,1,0,0};
-        int dc[4]={0,0,-1,1};
-        for(int i=0;i<4;i++)
-        {
-            int nr=dr[i];
-            int nc=dc[i];
-            if(!path(nr,nc,m,n))
-            continue;
-            ll cost=grid[0][0]+grid[nr][nc];
-            dist[nr][nc][i][0]=cost;
-            pq.push({cost,nr,nc,i,0});
-        }
-        while(!pq.empty())
-        {
-            auto[cost,r,c,prevDir,turns]=pq.top();
-            pq.pop();
-            if(cost!=dist[r][c][prevDir][turns])
-            continue;
-            if(r==m-1 && c==n-1)
-            return cost;
-            for(int i=0;i<4;i++)
-            {
-                int nr=r+dr[i];
-                int nc=c+dc[i];
-                if(!path(nr,nc,m,n))
-                continue;
-                int newTurns=turns;
-                if(i!=prevDir)
-                newTurns++;
-                if(newTurns>k)
-                continue;
-                ll newCost=cost+grid[nr][nc];
-                if(newCost<dist[nr][nc][i][newTurns])
-                {
-                    dist[nr][nc][i][newTurns]=newCost;
-                    pq.push({newCost,nr,nc,i,newTurns});
-                }
-            }
-        }
+        vector<vector<vector<vector<int>>>>dp(m,vector<vector<vector<int>>>(n,vector<vector<int>>(k+1,vector<int>(5,-1))));
+        int ans=f(0,0,k,4,grid,dp);
+        if(ans==INT_MAX)
         return -1;
+        return ans;
     }
 };
