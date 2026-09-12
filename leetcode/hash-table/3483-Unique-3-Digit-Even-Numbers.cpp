ee0ded206da64e8c869aef216d29c14f811d6eea
paavanf <pavu0504@gmail.com>
2026-09-12 21:02:48 +0530

    Add Unique-3-Digit-Even-Numbers (hash-table)

--- leetcode/hash-table/3483-Unique-3-Digit-Even-Numbers.cpp
+++ leetcode/hash-table/3483-Unique-3-Digit-Even-Numbers.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        set<int>s;
+        //for abc
+        //a!=0
+        //c must be even
+        //cannot use same copy
         int n=digits.size();
-        for(int i=0;i<n;i++){
-             if(digits[i]%2!=0) continue;
-            for(int j=0;j<n;j++){
-                if (j==i) continue;
-                if (digits[j]==0) continue;
-                for(int k=0;k<n;k++){
-                if (k==i || k==j) continue;
-
-                    int num=digits[j]*100+ digits[k]*10+digits[i];
-
-                    s.insert(num);
+        set<int>st;
+        for(int i=0;i<n;i++)
+        {
+            if(digits[i]==0)
+            continue;
+            for(int j=0;j<n;j++)
+            {
+                if(j==i)
+                continue;
+                for(int k=0;k<n;k++)
+                {
+                    if(k==i || k==j)
+                    continue;
+                    if(digits[k]%2!=0)
+                    continue;
+                    int num=digits[i]*100+digits[j]*10+digits[k];
+                    st.insert(num);
                 }
             }
         }
-        return s.size();
+        return st.size();
     }
 };
