e0add86da686d47ce7355239f5229e88454ecd5c
paavanf <pavu0504@gmail.com>
2026-09-21 19:23:19 +0530

    Add Find-X-Value-of-Array-I (dynamic-programming)

--- leetcode/dynamic-programming/3524-Find-X-Value-of-Array-I.cpp
+++ leetcode/dynamic-programming/3524-Find-X-Value-of-Array-I.cpp
@@ -1,28 +1,26 @@
-//1st way by not directly storing all the subarrays
+//2nd way through Dp
 using ll=long long;
 class Solution {
 public:
     vector<long long> resultArray(vector<int>& nums, int k) {
-        vector<ll>ans(k);
-        int fq[5]={0};//no of subarrays ending at the prev position whose product %k=x
+        vector<ll>ans(k,0);
+        vector<ll>dp(k,0);
         for(int n:nums)
         {
-            n%=k;
-            int currFq[5]={0};
-            currFq[n]=1;
-            //get all prev subarrays
+            int rem=n%k;
+            vector<ll>newDp(k,0);
+            newDp[rem]++;
             for(int x=0;x<k;x++)
             {
-                currFq[x*n%k]+=fq[x];
+                int newRem=(x*rem)%k;
+                newDp[newRem]+=dp[x];
             }
-            //add up the fq
             for(int x=0;x<k;x++)
             {
-                fq[x]=currFq[x];
-                ans[x]+=fq[x];
+                ans[x]+=newDp[x];
             }
+            dp=newDp;
         }
         return ans;
-
     }
 };
