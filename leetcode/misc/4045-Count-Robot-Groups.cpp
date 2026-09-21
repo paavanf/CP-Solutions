da8a09f33fc522f427da237a0e88db5d893360f9
paavanf <pavu0504@gmail.com>
2026-09-21 19:49:22 +0530

    Add Count-Robot-Groups (stack)

--- leetcode/misc/4045-Count-Robot-Groups.cpp
+++ leetcode/misc/4045-Count-Robot-Groups.cpp
@@ -1,19 +1,15 @@
-using ll=long long;
 class Solution {
 public:
     int countGroups(vector<int>& position, vector<int>& speed, int distance) {
-        int n=position.size();
-        ll grps=n;
-        ll maxiSpeed=speed[n-1];
-        for(int i=n-1;i>0;i--)
-        {
-            if(position[i]-position[i-1]<=distance)
-            grps--;
-            else if(speed[i-1]>maxiSpeed)
-            grps--;
-            else
-            maxiSpeed=speed[i-1];
+        int result = 1, n = position.size(), lastSpeed = speed[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            if (position[i + 1] - position[i] <= distance || speed[i] > lastSpeed) {
+                continue;
+            } else {
+                result++, lastSpeed = speed[i];
+            }
         }
-        return (int)grps;
+
+        return result;
     }
 };
