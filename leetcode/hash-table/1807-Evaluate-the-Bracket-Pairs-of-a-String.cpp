8bd8e8eb8a3922792c6591b38a303049e04debbb
paavanf <pavu0504@gmail.com>
2026-09-26 20:37:06 +0530

    Add Evaluate-the-Bracket-Pairs-of-a-String (hash-table)

---
+++ leetcode/hash-table/1807-Evaluate-the-Bracket-Pairs-of-a-String.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+        unordered_map<string,string>mp;
+        for(auto &x:knowledge)
+        mp[x[0]]=x[1];
+        string ans;
+        int n=s.size();
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(')
+            {
+                int j=i+1;
+                while(s[j]!=')')
+                j++;
+                string key=s.substr(i+1,j-i-1);
+                if(mp.count(key))
+                ans+=mp[key];
+                else
+                ans+='?';
+                i=j;
+            }
+            else
+            ans+=s[i];
+        }
+        return ans;
+    }
+};
